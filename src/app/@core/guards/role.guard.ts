8a5fbd255953a0514d7f1d4166240a236f346946
bhagattamu <bhagattamu@gmail.com>
2021-05-07 00:08:55 +0545

    Added autoPassword check

--- src/app/@core/guards/role.guard.ts
+++ src/app/@core/guards/role.guard.ts
@@ -10,7 +10,7 @@ export class RoleGuard implements CanActivate {
     canActivate(route: ActivatedRouteSnapshot): Observable<boolean> | boolean {
         const definedRole = route.data.role;
         const currentUser = this.authService.getUserData();
-        if (currentUser && currentUser.roles && currentUser.roles.some((role: string) => definedRole.includes(role))) {
+        if (currentUser && !currentUser.autoPassword && currentUser.roles && currentUser.roles.some((role: string) => definedRole.includes(role))) {
             return true;
         }
         return false;
